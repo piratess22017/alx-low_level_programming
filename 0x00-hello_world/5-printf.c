b8767c6f32a0b1a2cbf7bbd61e5a42f2afdcc234
Ephrem <medu0955@gmail.com>
2023-02-16 15:46:15 -0800

    5 commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/** a program tha prints letter followed by
+ ** new line
+ **/
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
