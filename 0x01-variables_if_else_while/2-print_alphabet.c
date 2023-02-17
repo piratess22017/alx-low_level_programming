927c53020e8bd3eb258b1e70b50b95dc6557732b
Ephrem <medu0955@gmail.com>
2023-02-17 15:10:18 -0800

    2 commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints all the alphabets in small letter
+ * all the 26 letters
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar("abcdefghijklmnopqrstuvwxyz\n");
+	return (0);
+}
